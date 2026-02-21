bbc5b60877c9f2e330182dbe92f0885b1ec194d0
madisettyrangavijayalakshmi-create <madisettyrangavijayalakshmi@gmail.com>
2026-02-21 11:51:44 +0530

    Add files via upload

---
+++ perfect num.py
@@ -0,0 +1,10 @@
+n = int(input())
+sum_div = 0
+for i in range(1, n):
+    if n % i == 0:
+        sum_div += i
+    if sum_div == n:
+        print("perfect number")
+    else:
+        print("not a perfect number")
+   
