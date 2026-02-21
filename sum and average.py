55d6c898f55ca9860d3519ee4a0574eb0f524b32
madisettyrangavijayalakshmi-create <madisettyrangavijayalakshmi@gmail.com>
2026-02-21 11:48:21 +0530

    Add files via upload

---
+++ sum and average.py
@@ -0,0 +1,8 @@
+a= int(input("enter a value"))
+b = int(input("enter b value"))
+        
+c = int(input("enter c value"))
+total = a + b + c
+average = total/3
+print("total:",total)
+print("Average:",average)       
