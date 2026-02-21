7038453d225879948137c270772b06be0219ad78
madisettyrangavijayalakshmi-create <madisettyrangavijayalakshmi@gmail.com>
2026-02-21 11:55:23 +0530

    Add files via upload

---
+++ substring.py
@@ -0,0 +1,6 @@
+main_string = input("enter main string")
+sub_string = input("enter sub string")
+if sub_string in main_string:
+    print("substring is present")
+else:
+    print("substring is not present") 
